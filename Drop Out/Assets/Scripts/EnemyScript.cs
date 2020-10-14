f07d6bcd33db7ad002e5f9a5cb74633f19918677
Ankoopa <dmdasalla@mcm.edu.ph>
2020-10-14 16:15:47 +0800

    added enemy pathfinding

---
+++ Drop Out/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.AI.NavMeshAgent navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        navAgent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
